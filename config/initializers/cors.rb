a90aaa5b84e810b9765a6bf7a22389db0acc6735
寺川航平 <odayakalife@ods-MacBook-Pro.local>
2021-02-04 16:32:50 +0900

    add cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:8080','https://order-mgt.herokuapp.com'
+      origins 'http://localhost:8080','https://order-mgt.herokuapp.com'
   
-  
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
     end
-  end
+end
